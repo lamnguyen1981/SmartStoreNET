1050096dd5e1b201353cdc57e8e735fd33718a35
Hiep <Hiep@completecoders.com>
2022-04-13 19:14:51 +0700

    missing file

---
+++ src/Plugins/CC.Plugins.Subscription/Models/MarketOffersResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CC.Plugins.Subscription.Models
+{
+    public class MarketOffersResponse
+    {
+        public string Vehicle { get; set; }
+
+        public string OfferCode { get; set; }
+
+        public string OfferDescription { get; set; }
+
+        public string DefaultAmount { get; set; }
+
+        public string MarketRecommendation { get; set; }
+    }
+}
